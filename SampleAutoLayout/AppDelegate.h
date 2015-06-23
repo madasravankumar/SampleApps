fc4162f8c469d0fe3482fb4475127b8732c3be4e
sravan <sravan143.m@gmail.com>
2015-06-23 19:55:44 +0530

    Initial Commit

---
+++ SampleAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleAutoLayout
+//
+//  Created by Gemini-iMac2 on 23/06/15.
+//  Copyright (c) 2015 Gemini-iMac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleAutoLayout
+//
+//  Created by Gemini-iMac2 on 23/06/15.
+//  Copyright (c) 2015 Gemini-iMac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
