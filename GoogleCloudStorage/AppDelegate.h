a6feb32d7033386d637acd9e2d551df7022ffc89
sravan <sravankumar143>
2015-07-15 16:41:13 +0530

    Initial Commit

---
+++ GoogleCloudStorage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleCloudStorage
+//
+//  Created by Gemini-iMac2 on 15/07/15.
+//  Copyright (c) 2015 Gemini-iMac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
