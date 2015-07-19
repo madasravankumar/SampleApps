d1640f57b815abe1dc2e931066df3da5179842f9
sravankumar143 <madasravankumar@gmail.com>
2015-07-19 18:37:45 +0530

    Initial Commit

---
+++ GetLocaton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetLocaton
+//
+//  Created by apple on 19/07/15.
+//  Copyright (c) 2015 globalnest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetLocaton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetLocaton
+//
+//  Created by apple on 19/07/15.
+//  Copyright (c) 2015 globalnest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
